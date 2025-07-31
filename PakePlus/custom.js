84027780480f756de533b2b298f9b00f4c5dd9d9
zxc4we-lab <zxc4we028@gmail.com>
2025-07-31 22:08:23 +0700

    update custom js from pakeplus

--- PakePlus/custom.js
+++ PakePlus/custom.js
@@ -4,7 +4,7 @@ console.log(
 )
 
 // very important, if you don't know what it is, don't touch it
-// 非常重要，不懂代码不要动
+// 非常重要，不懂代码不要动，这里可以解决80%的问题，也可以生产1000+的bug
 const hookClick = (e) => {
     const origin = e.target.closest('a')
     const isBaseTargetBlank = document.querySelector(
@@ -23,4 +23,31 @@ const hookClick = (e) => {
     }
 }
 
+window.open = function (url, target, features) {
+    console.log('open', url, target, features)
+    location.href = url
+}
+
 document.addEventListener('click', hookClick, { capture: true })
+
+// css filter
+document.addEventListener('DOMContentLoaded', () => {
+    const targetNode = document.body
+    // 配置观察选项
+    const config = {
+        childList: true,
+        subtree: true,
+    }
+    const observer = new MutationObserver((mutationsList, observer) => {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                const element0 = document.querySelector('Create AI Music');
+                if (element0) {
+                    element0.style.display = 'none';
+                }
+            }
+        }
+    })
+    observer.observe(targetNode, config)
+})
+// end css filter
